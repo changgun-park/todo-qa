ee0163138437fa077aaff667d5870ab3288dd0bb
changgun-park <pcg0537@gmail.com>
2024-10-15 12:03:20 +0900

    chores: todo list add / toggle / delete fn 추가ㅇ

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,87 @@
+import { useEffect, useState } from "react";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 function App() {
-  return <div>Hello world</div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const addTodo = () => {
+    const newTodo = {
+      title: input,
+      completed: false,
+    };
+
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTodo),
+    })
+      .then((res) => res.json())
+      .then((data) => setTodos([...todos, data]))
+      .catch((err) => console.log(err));
+  };
+
+  const toggleComplete = (id: number) => {
+    fetch(`http://localhost:3000/todos/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        setTodos((prev) => prev.map((todo) => (todo.id === id ? data : todo)))
+      );
+  };
+
+  const deleteTodo = (id: number) => {
+    fetch(`http://localhost:3000/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => setTodos(todos.filter((todo) => todo.id !== id)));
+  };
+
+  return (
+    <div>
+      <h1>Todo list</h1>
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{ textDecoration: todo.completed ? "line-through" : "" }}
+          >
+            {todo.title}
+            <button onClick={() => toggleComplete(todo.id)}>
+              {todo.completed ? "Undo" : "Complete"}
+            </button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
